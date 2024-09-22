1f80624239979f3ae290974747dead63745e38cc
linh-kieu <kieungoclinh.304@gmail.com>
2024-09-22 12:14:46 -0400

    Create practice_02.sql

---
+++ practice_02.sql
@@ -0,0 +1,17 @@
+--ex1: hackerrank-weather-observation-station-3.
+SELECT DISTINCT city FROM station
+WHERE  id LIKE '%0'OR id LIKE '%2' OR id LIKE '%4' OR id LIKE '%6' OR id LIKE '%8';
+
+--ex2: hackerrank-weather-observation-station-4.
+
+
+--ex3: hackerrank-the-blunder.
+--ex4: datalemur-alibaba-compressed-mean.
+--ex5: datalemur-matching-skills.
+--ex6: datalemur-verage-post-hiatus-1.
+--ex7: datalemur-cards-issued-difference.
+--ex8: datalemur-non-profitable-drugs.
+--ex9: leetcode-not-boring-movies.
+--ex10: leetcode-number-of-unique-subject.
+--ex11: leetcode-find-followers-count.
+--ex12:leetcode-classes-more-than-5-students.
