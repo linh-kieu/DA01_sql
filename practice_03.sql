25cfee23ec7976760fafb65edf9991e78f152f2a
linh-kieu <kieungoclinh.304@gmail.com>
2024-09-30 01:31:13 -0400

    Update practice_03.sql

--- practice_03.sql
+++ practice_03.sql
@@ -1,1 +1,19 @@
+--ex1: hackerrank-more-than-75-marks.
+SELECT name FROM students
+WHERE marks >75
+ORDER BY RIGHT(name,3), ID;
 
+--ex2: leetcode-fix-names-in-a-table.
+SELECT user_id, 
+UPPER(LEFT(name,1))||LOWER(SUBSTRING(name FROM 2 FOR 10)) AS name
+FROM users
+ORDER BY user_id;
+
+--ex3: datalemur-total-drugs-sales.
+--ex4: avg-review-ratings.
+--ex5: teams-power-users.
+--ex6: invalid-tweets.
+--ex7: user-activity-for-the-past-30-days.
+--ex8: number-of-hires-during-specific-time-period.
+--ex9: positions-of-letter-a.
+--ex10: macedonian-vintages.
